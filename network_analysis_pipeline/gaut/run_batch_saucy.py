750825a38709827ba1b907ab0f805f471ff64024
jonward1982 <j.a.ward@leeds.ac.uk>
2024-04-22 10:34:18 +0100

    Updated run_batch_saucy.py.

--- network_analysis_pipeline/gaut/run_batch_saucy.py
+++ network_analysis_pipeline/gaut/run_batch_saucy.py
@@ -14,24 +14,25 @@ import subprocess
 import time
 start_time = time.time()
 
-# Directory path
-stub='../graphs/format_scy/test_examples/'
+# Directory name and path
+dname='test_examples/'
+dstub='../graphs/format_scy/'
+stub=dstub+dname
+
 
 count=1
 # Loop
 for dirpath, dirnames, files in os.walk(stub):
     if 'conversion_code' not in dirpath:
-        dname=dirpath.lstrip('../graphs/')
         if len(dname)>0 and not os.path.exists(dname):
             print(f'Make directory: {dname}')
             os.makedirs(dname)        
-        if len(dirnames)==0:        
-            pp=dirpath.lstrip('../graphs/')
+        if len(dirnames)==0:     
             for fname in files:
                 fnamestub=fname.replace('.scy','')
-                if fname[0]!='.' and not os.path.isfile(os.path.join(pp,fnamestub)+'.gaut'):                    
-                    fgaut=open(os.path.join(pp,fnamestub)+'.gaut','w')
-                    flog=open(os.path.join(pp,fnamestub)+'.log','w')
+                if fname[0]!='.' and not os.path.isfile(os.path.join(dname,fnamestub)+'.gaut'):                    
+                    fgaut=open(os.path.join(dname,fnamestub)+'.gaut','w')
+                    flog=open(os.path.join(dname,fnamestub)+'.log','w')
                     print('{0:6}\t Processing: {1:20}\t Runtime: {2:e}'.format(count,fnamestub,time.time() - start_time))
                     p=subprocess.Popen(['saucy','-s',os.path.join(dirpath,fname)],stdout=subprocess.PIPE,stderr=subprocess.PIPE)
                     output, error = p.communicate()
@@ -42,4 +43,8 @@ for dirpath, dirnames, files in os.walk(stub):
                 
                     count+=1
 
-print("--- {0} seconds ---".format(time.time() - start_time))
+                elif fname[0]!='.':
+                    print('{0:6}\t Existing file: {1:20}\t Runtime: {2:e}'.format(count,fnamestub,time.time() - start_time))
+                    
+                    
+print("--- Total time: {0} seconds ---".format(time.time() - start_time))
